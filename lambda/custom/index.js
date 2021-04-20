ace089642ab85d9b92cdf4d00b114bb5de882760
Cristiane Santiago <cris.s.santiago@gmail.com>
2021-04-20 15:21:24 -0400

    Added the method to answer the first question

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -3,6 +3,16 @@
 
 const Alexa = require('ask-sdk-core');
 
+// This method is for Alexa's first presentation.
+const GetHelloHandler = {
+  canHandle(handlerInput) { return handlerInput.requestEnvelope.request.type === "LaunchRequest"; },
+  // Returns the created outputSpeech constant.
+  handle(handlerInput) {
+    const outputSpeech = "Hello, my name is Alexa. Welcome to the Rocket Elevetors System. How can I help you today?";
+    return handlerInput.responseBuilder.speak(outputSpeech).reprompt().getResponse();
+  }
+};
+
 const GetRemoteDataHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'LaunchRequest'
@@ -26,6 +36,46 @@ const GetRemoteDataHandler = {
     return handlerInput.responseBuilder
       .speak(outputSpeech)
       .getResponse();
+  },
+};
+
+// The method returns a summary of Rocket Elevator's activities, when asked "Hey Alexa, What is going on at Rocket Elevators?"
+const GetInfoHandler = {
+  canHandle(handlerInput) {
+    return (handlerInput.requestEnvelope.request.type === 'IntentRequest' 
+    && handlerInput.requestEnvelope.request.intent.name === 'GetInfoIntent'); },
+  async handle(handlerInput) {
+    let outputSpeech = 'This is the default message.';
+    
+    // API calls to get information
+    const listOfElevators = await getRemoteData('https://rocket-elevators-ai.azurewebsites.net/api/elevators')
+    const listElevatorNotActive = await getRemoteData('https://rocket-elevators-ai.azurewebsites.net/api/elevators/NotActive')
+    const listOfBuildings = await getRemoteData('https://rocket-elevators-ai.azurewebsites.net/api/buildings')
+    const listOfCustomers =  await getRemoteData('https://rocket-elevators-ai.azurewebsites.net/api/customers')
+    const listOfBatteries = await getRemoteData('https://rocket-elevators-ai.azurewebsites.net/api/batteries/')    
+    const listOfCity = await getRemoteData('https://rocket-elevators-ai.azurewebsites.net/api/Addresses/City')
+    const listOfQuote = await getRemoteData('https://rocket-elevators-ai.azurewebsites.net/api/quotes')    
+    const listOfLead = await getRemoteData('https://rocket-elevators-ai.azurewebsites.net/api/leads')
+    
+    // Parsing the GET requests as JSON
+    const amountOfElevators = JSON.parse(listOfElevators).length
+    const amountOfElevatorNotActive = JSON.parse(listElevatorNotActive).length
+    const amountOfBuildings = JSON.parse(listOfBuildings).length
+    const amountOfCustomers = JSON.parse(listOfCustomers).length
+    const amountOfBatteries = JSON.parse(listOfBatteries).length
+    const amountOfCitys = JSON.parse(listOfCity).length
+    const amountOfQuotes = JSON.parse(listOfQuote).length
+    const amountOfLeads = JSON.parse(listOfLead).length   
+    
+    // Creating the voice output with the quantity returned from the APIs 
+    outputSpeech = `Greetings, there are currently ${amountOfElevators} elevators deployed in the ${amountOfBuildings} buildings
+    of your ${amountOfCustomers} customers. Currently, ${amountOfElevatorNotActive} are not in Running Status and are being serviced.
+    ${amountOfBatteries} Batteries are deployed across ${amountOfCitys} cities. On another note, you currently have ${amountOfQuotes} 
+    quotes awaiting processing. You also have ${amountOfLeads} leads in your contact requests.`;
+    
+    // Returning the output speech
+    return handlerInput.responseBuilder.speak(outputSpeech).getResponse();
+    
   },
 };
 
@@ -101,7 +151,8 @@ const skillBuilder = Alexa.SkillBuilders.custom();
 
 exports.handler = skillBuilder
   .addRequestHandlers(
-    GetRemoteDataHandler,
+    GetHelloHandler,
+    GetInfoHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler,
